61755e9c3e7e6aa62797ea2eca19833ec2066a56
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-01-20 23:31:25 +0900

    🎨 [#1] Pretendard 글꼴을 로컬 글꼴로 적용한다

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,59 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+
 import { ILayoutProps } from '@/src/interfaces';
+
 import './globals.css';
 
 export const Pretendard = localFont({
-  src: '../fonts/woff2/PretendardVariable.woff2',
+  src: [
+    {
+      path: '../../public/fonts/Pretendard-Black.subset.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-ExtraBold.subset.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Bold.subset.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-SemiBold.subset.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Medium.subset.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Regular.subset.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Light.subset.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-ExtraLight.subset.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Thin.subset.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+  ],
   display: 'swap',
-  variable: '--font-pretendard',
   preload: true,
 });
 
