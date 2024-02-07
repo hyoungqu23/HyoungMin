3ed6bd3ede90798462c4593d6494f66811f35b9d
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-02-07 23:45:44 +0900

    🎨 [#7] Article 페이지를 추가한다

---
+++ src/app/articles/[slug]/page.tsx
@@ -0,0 +1,56 @@
+import { allArticles } from '@contentlayer';
+import { useMDXComponent } from 'next-contentlayer/hooks';
+import { notFound } from 'next/navigation';
+import Image from 'next/image';
+
+interface IArticlePageProps {
+  params: { slug: string };
+}
+
+const ArticlePage = ({ params: { slug } }: IArticlePageProps) => {
+  const article = allArticles.find(article => article.slugAsParams === slug);
+
+  if (!article) notFound();
+
+  const MDXContent = useMDXComponent(article.body.code);
+
+  return (
+    <section className='flex flex-col px-4 py-10 items-center'>
+      <>
+        {article.thumbnail ? (
+          <Image
+            src={article.thumbnail}
+            alt={article.title}
+            width={400}
+            height={300}
+            className='object-contain'
+          />
+        ) : null}
+      </>
+      <h1 className='text-display2 font-extrabold'>{article.title}</h1>
+      <p>{article.description}</p>
+      <p>{new Date(article.createdAt).toLocaleDateString()}</p>
+      <p>{article.category}</p>
+
+      <ul>
+        {article.tags.map(tag => (
+          <span key={tag}>{tag}</span>
+        ))}
+      </ul>
+      <div className='w-full h-0.5 bg-primary-500 my-4' />
+      <article className='prose prose-primary max-w-3xl'>
+        <MDXContent />
+      </article>
+    </section>
+  );
+};
+
+export default ArticlePage;
+
+export const generateStaticParams = async () => {
+  return allArticles.map(article => {
+    return {
+      slug: article._raw.flattenedPath,
+    };
+  });
+};
