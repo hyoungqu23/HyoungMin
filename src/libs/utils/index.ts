24f9c2cb63a03b7398870651589f645aa7ede3d7
HyoungMin <hyoungqu23@gmail.com>
2024-02-10 22:33:18 +0900

    🎨 [#7] 랜덤으로 색상을 설정하는 함수를 추가한다

--- src/libs/utils/index.ts
+++ src/libs/utils/index.ts
@@ -5,3 +5,15 @@ export const cls = (
     .filter((className) => className && typeof className === 'string')
     .join(' ');
 };
+
+export const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+
+  let color = '#';
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+};
