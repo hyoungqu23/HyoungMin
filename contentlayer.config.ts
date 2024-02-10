f1bf939d30b009ce946eb80ecc36813654ab714a
HyoungMin <hyoungqu23@gmail.com>
2024-02-10 21:07:01 +0900

    🔧 [#2] `isRecommended` 필드를 Contentlayer 헤더 타입에 추가한다

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -34,6 +34,11 @@ export const Article = defineDocumentType(() => ({
       type: 'string',
       required: false,
     },
+    isRecommended: {
+      type: 'boolean',
+      required: false,
+      default: false,
+    },
   },
   computedFields: {
     slug: {
