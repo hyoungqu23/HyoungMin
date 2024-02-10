7e86adc82fcb523dff20b4a435ce04298096e0dc
HyoungMin <hyoungqu23@gmail.com>
2024-02-10 22:28:31 +0900

    🎨 [#7] 아티클을 시간 순으로 정렬하여 렌더링한다

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -3,10 +3,14 @@ import { articleService } from '@/src/services/article';
 import { allArticles } from '@contentlayer';
 
 const ArticlesPage = () => {
+  const sortedArticles = allArticles.sort(
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+  );
+
   return (
     <section className='flex flex-col items-center justify-between px-4 py-10 tablet:py-40'>
-      <section className='columns-2 gap-8'>
-        {allArticles.map(article => (
+      <section className='flex flex-wrap gap-8 w-full justify-center'>
+        {sortedArticles.map((article) => (
           <Card
             key={article._id}
             articlePreview={articleService.getPreview(article)}
