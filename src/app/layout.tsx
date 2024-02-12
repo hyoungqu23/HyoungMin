b6199c4d70f1ece000082352dc856660888f3950
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-02-13 06:38:01 +0900

    📈 [#9] Vercel의 Analytics 컴포넌트를 추가한다

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
@@ -83,6 +84,7 @@ const RootLayout = ({ children }: ILayoutProps) => {
           {children}
         </main>
         <Footer />
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
