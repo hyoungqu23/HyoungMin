c02b434382cc331e2f36aef95d04e9591a530f07
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-02-13 06:04:27 +0900

    📈 [#9] TrackingTools 컴포넌트를 생성하고, MicroSoft Clarity 추적 코드를 추가한다

---
+++ src/components/common/TrackingTools/index.tsx
@@ -0,0 +1,20 @@
+import Script from 'next/script';
+
+const TrackingTools = () => {
+  return (
+    <>
+      <Script
+        id='microsoft-clarity-script'
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY_TRACK_CODE}");`,
+        }}
+      />
+    </>
+  );
+};
+
+export default TrackingTools;
