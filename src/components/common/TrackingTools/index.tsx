95baa9fd2eb61a31cd18ecf81d9d1d9b3e3ba003
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-02-13 06:04:49 +0900

    📈 [#9] TrackingTools 컴포넌트에 Google Analytics 추적 코드를 추가한다

--- src/components/common/TrackingTools/index.tsx
+++ src/components/common/TrackingTools/index.tsx
@@ -13,6 +13,19 @@ const TrackingTools = () => {
           })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY_TRACK_CODE}");`,
         }}
       />
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_PROPERTY_ID}`}
+      ></Script>
+      <Script
+        id='google-analytics-script'
+        dangerouslySetInnerHTML={{
+          __html: ` window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_PROPERTY_ID}');`,
+        }}
+      />
     </>
   );
 };
