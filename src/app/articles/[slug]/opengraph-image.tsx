eefc3a1a2a75010bc5031c11432c1f771f1541c3
HyoungMin <hyoungqu23@gmail.com>
2024-02-14 08:37:30 +0900

    🔍 [#8] Next.js 14버전 이후 추가된 OpenGraph, Twitter 이미지를 생성하는 로직을 추가한다

---
+++ src/app/articles/[slug]/opengraph-image.tsx
@@ -0,0 +1,42 @@
+import { allArticles } from '@contentlayer';
+import { ImageResponse } from 'next/og';
+
+export const alt = 'HyoungMin Tech Blog Article';
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = 'image/png';
+
+interface IOpenGraphImageProps {
+  params: { slug: string };
+}
+
+const Image = async ({ params: { slug } }: IOpenGraphImageProps) => {
+  const article = allArticles.find((article) => article.slugAsParams === slug);
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {article?.title ?? 'HyoungMin Tech Blog'}
+      </div>
+    ),
+    {
+      ...size,
+    },
+  );
+};
+
+export default Image;
+
+export const runtime = 'edge';
