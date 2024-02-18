c8503affe880fafa2df551cc47fe89eca6863c6c
HyoungMin <hyoungqu23@gmail.com>
2024-02-19 08:49:01 +0900

    🔍 [#8] articles를 활용해 sitemap을 동적으로 생성한다

---
+++ src/app/sitemap.ts
@@ -0,0 +1,12 @@
+import { BASE_URL } from '@/src/assets/constants';
+import { allArticles } from '@contentlayer';
+import type { MetadataRoute } from 'next';
+
+const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
+  return allArticles.map((article) => ({
+    url: `${BASE_URL}/${article._raw.flattenedPath}`,
+    lastModified: article.createdAt,
+  }));
+};
+
+export default sitemap;
