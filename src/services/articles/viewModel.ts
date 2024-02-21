185bb4e779626eb2b2ec5bc280fa7b27ed9b4247
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-02-22 06:54:34 +0900

    🎨 [#6] Article View Model을 추가하고, getPreview 메서드와 Filter 메서드를 추가한다

---
+++ src/services/articles/viewModel.ts
@@ -0,0 +1,16 @@
+import { IArticle, IArticleAdapter } from '@/src/services/articles/model';
+import { allArticles } from '@contentlayer';
+
+class ArticleViewModel {
+  private articles: Array<IArticle> = [];
+
+  constructor(private adapter: IArticleAdapter) {
+    this.articles = allArticles;
+  }
+
+  getPreview = (articles: Array<IArticle>) =>
+    articles.map(this.adapter.toPreview);
+
+  filter = (filterBy: string) =>
+    this.articles.filter(article => article.category === filterBy);
+}
