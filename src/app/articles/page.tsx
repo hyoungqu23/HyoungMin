7280aa51baf97005e581ae8e40f91390c7ed005d
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-02-22 23:58:51 +0900

    ♻️ [#6] ArticleViewModel을 활용해 Articles Page의 UI 렌더링 로직을 리팩토링한다

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,16 +1,14 @@
 import Card from '@/src/components/ui/Card';
-import { articleService } from '@/src/services/article';
-import { allArticles } from '@contentlayer';
+import { ArticleAdapter } from '@/src/services/articles/adapter';
+import { ArticleViewModel } from '@/src/services/articles/viewModel';
 
 const ArticlesPage = () => {
-  const sortedArticles = allArticles.sort(
-    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
-  );
+  const articleService = new ArticleViewModel(new ArticleAdapter());
 
   return (
     <section className='flex flex-col items-center justify-between px-4 py-10 tablet:py-40'>
       <section className='flex flex-wrap gap-8 w-full justify-center'>
-        {sortedArticles.map((article) => (
+        {articleService.sortByCreatedAt().map(article => (
           <Card
             key={article._id}
             articlePreview={articleService.getPreview(article)}
