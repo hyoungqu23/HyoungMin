9c187c4cc8a0484da1c1430f48479b9a617b685e
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-01 01:26:53 +0900

    🚚 [#14] FSD의 Apps Layer를 적용하여 기존 디렉토리 구조와 파일을 마이그레이션한다
    - Apps Layer는 가장 추상화되지 않은 레이어로, 대부분 재사용되지 않으나 어플리케이션 실행 시 전체적으로 영향을 미치는 모듈이 존재한다

--- src/layers/apps/layout/ui/Header/index.tsx
+++ src/layers/apps/layout/ui/Header/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { NAVIGATION_ITEMS } from '@/src/assets/constants';
+import { NAVIGATION_ITEMS } from '@shared';
 import Link from 'next/link';
 
-const Header = () => {
+export const Header = () => {
   return (
     <header className='sticky z-50 top-0 flex items-center justify-center w-screen h-16 bg-secondary-900/50 backdrop-blur-sm bg-clip-padding backdrop-filter'>
       <nav className='flex gap-8'>
-        {Object.values(NAVIGATION_ITEMS).map(item => (
+        {Object.values(NAVIGATION_ITEMS).map((item) => (
           <Link
             key={item.id}
             href={item.route}
@@ -19,5 +18,3 @@ const Header = () => {
     </header>
   );
 };
-
-export default Header;
