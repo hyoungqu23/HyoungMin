1765aadfc86440be36051027668d2afb76d2ca91
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-01 01:34:56 +0900

    🔥 [#12] contentlayer 관련 설정을 제거한다

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-const { withContentlayer } = require('next-contentlayer');
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = { swcMinify: true };
 
-module.exports = withContentlayer(nextConfig);
+module.exports = nextConfig;
