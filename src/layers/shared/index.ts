985b59392457fe91fe23404aab44611ffe1dd18d
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-01 01:24:47 +0900

    🚚 [#14] FSD의 Shared Layer를 적용하여 기존 디렉토리 구조와 파일을 마이그레이션한다
    - Shared Layer는 가장 강하게 추상화된 레이어로, 공통 로직과 UI가 존재한다
    - 모든 레이어에서 활용되며, 비즈니스 로직이 존재하지 않으므로 슬라이스로 구분되지 않으며 레이어 자체만으로 하나의 슬라이스가 되어 바로 하위에 직접 세그먼트를 갖게 된다

---
+++ src/layers/shared/index.ts
@@ -0,0 +1,5 @@
+// export * from './api';
+export * from './asset/constants';
+export * from './lib/types';
+export * from './lib/utils';
+export * from './ui';
--- src/layers/shared/lib/utils/index.ts
+++ src/layers/shared/lib/utils/index.ts
@@ -2,7 +2,7 @@ export const cls = (
   ...classNames: Array<string | boolean | null | undefined>
 ): string => {
   return classNames
-    .filter(className => className && typeof className === 'string')
+    .filter((className) => className && typeof className === 'string')
     .join(' ');
 };
 
