040dd3a14410229d12d5d0c906506673b6f435ac
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-07 23:32:03 +0900

    ✨ [#12] `ArticleService`에서 활용되는 내부 메서드 `_get`, `_parse`를 생성한다

--- src/layers/entities/articles/model/index.ts
+++ src/layers/entities/articles/model/index.ts
@@ -1,10 +1,34 @@
 import {
+  CONTENTS_PATH_MAP,
+  getRootDirectoryPath,
   type TMetadata,
 } from '@shared';
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+
 type TArticleService = {
   _parse: (file: string) => { data: TMetadata; content: string };
   _get: (slug: string) => string;
   getPreview: (slug: string) => TMetadata;
   getContent: (slug: string) => string;
   getReadingTime: (content: string) => number;
+};
+
+export const articleService: TArticleService = {
+  _parse: (articleFile) => {
+    const { data, content } = matter<TMetadata, string, {}>(articleFile);
+
+    return { data, content };
+  },
+  _get: (slug) => {
+    const articlePath = path.join(
+      getRootDirectoryPath(),
+      CONTENTS_PATH_MAP.ROOT,
+      CONTENTS_PATH_MAP.ARTICLES,
+      `${slug}.mdx`,
+    );
+
+    return fs.readFileSync(articlePath, 'utf-8');
+  },
 };
