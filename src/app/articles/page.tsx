4b897861db0b4583858beebfa3c51c6d6e66fb58
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-09 06:50:30 +0900

    ♻️ [#12] CMS 변경과 개선된 articlesService를 적용해 전체 아티클 페이지를 리팩토링한다

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,19 +1,18 @@
-import Card from '@/src/components/ui/Card';
-import { ArticleAdapter } from '@/src/services/articles/adapter';
-import { ArticleViewModel } from '@/src/services/articles/viewModel';
+import { notFound } from 'next/navigation';
+import { articlesService } from '@entities';
 
 const ArticlesPage = () => {
-  const articleService = new ArticleViewModel(new ArticleAdapter());
+  const articles = articlesService.get();
 
   return (
     <section className='flex flex-col items-center justify-between px-4 py-10 tablet:py-40'>
       <section className='flex flex-wrap gap-8 w-full justify-center'>
-        {articleService.sortByCreatedAt().map(article => (
+        {/* {articleService.sortByCreatedAt().map((article) => (
           <Card
             key={article._id}
             articlePreview={articleService.getPreview(article)}
           />
-        ))}
+        ))} */}
       </section>
     </section>
   );
