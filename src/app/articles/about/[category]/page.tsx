d96f5685871249a4f71d9bbb1d47819362187ebe
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-09 06:50:14 +0900

    ♻️ [#12] CMS 변경과 개선된 articlesService를 적용해 카테고리 페이지를 리팩토링한다

--- src/app/articles/about/[category]/page.tsx
+++ src/app/articles/about/[category]/page.tsx
@@ -1,39 +1,21 @@
-import Card from '@/src/components/ui/Card';
-import { ArticleAdapter } from '@/src/services/articles/adapter';
-import { ArticleViewModel } from '@/src/services/articles/viewModel';
-import { notFound } from 'next/navigation';
+import { articlesService } from '@entities';
 
 interface ICategoryPageProps {
   params: { category: string };
 }
 
 const CategoryPage = ({ params: { category } }: ICategoryPageProps) => {
-  const articleService = new ArticleViewModel(new ArticleAdapter());
-
-  const categories = articleService.getCategories();
-
-  if (!categories.map(category => category.toLowerCase()).includes(category)) {
-    notFound();
-  }
-
-  const categoryInArticle = categories.find(c => c.toLowerCase() === category);
-
-  if (!categoryInArticle) {
-    notFound();
-  }
-
-  const articlesInCategory = articleService.filterByCategory(categoryInArticle);
+  const articlesInCategory = articlesService.filter(category);
 
   return (
     <section className='flex flex-col items-center justify-between px-4 py-10 tablet:py-40'>
-
       <section className='flex flex-wrap gap-8 w-full justify-center'>
-        {articlesInCategory.map(article => (
+        {/* {articlesInCategory.map((article) => (
           <Card
             key={article._id}
             articlePreview={articleService.getPreview(article)}
           />
-        ))}
+        ))} */}
       </section>
     </section>
   );
