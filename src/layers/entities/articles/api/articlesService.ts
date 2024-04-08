c6f171dba1b044c00a839927c7a818e6ae924b01
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-09 06:38:44 +0900

    ♻️ [#14] 인터페이스에 맞는 articlesService를 추가한다

---
+++ src/layers/entities/articles/api/articlesService.ts
@@ -0,0 +1,24 @@
+import { CONTENTS_PATH_MAP, getRootDirectoryPath } from '@shared';
+import fs from 'fs';
+import path from 'path';
+import { TArticlesService } from '../model';
+import { articleService } from './articleService';
+
+export const articlesService: TArticlesService = {
+  get: () => {
+    const articlesPath = path.join(
+      getRootDirectoryPath(),
+      CONTENTS_PATH_MAP.ROOT,
+      CONTENTS_PATH_MAP.ARTICLES,
+    );
+
+    const slugOfArticles = fs.readdirSync(articlesPath);
+
+    return slugOfArticles.map((slug) => articleService.getPreview(slug));
+  },
+  filter: (filterBy) => {
+    const articles = articlesService.get();
+
+    return articles.filter((article) => article.category === filterBy);
+  },
+};
