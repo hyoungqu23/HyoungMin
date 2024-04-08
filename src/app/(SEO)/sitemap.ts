443c726cfc1a812af41c14873bc8457be7157e1d
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-09 06:49:47 +0900

    ♻️ [#12] CMS 변경과 개선된 articlesService를 적용해 리팩토링 한다

--- src/app/(SEO)/sitemap.ts
+++ src/app/(SEO)/sitemap.ts
@@ -1,10 +1,12 @@
+import { articlesService } from '@entities';
 import { BASE_URL } from '@shared';
-import { allArticles } from '@contentlayer';
 import type { MetadataRoute } from 'next';
 
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
-  return allArticles.map((article) => ({
-    url: `${BASE_URL}/${article._raw.flattenedPath}`,
+  const articles = articlesService.get();
+
+  return articles.map((article) => ({
+    url: `${BASE_URL}/${article.slug}`,
     lastModified: article.createdAt,
   }));
 };
