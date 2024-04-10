0003b6c208dd0d42c2d4beed9794e62b05259c4e
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-10 13:14:48 +0900

    🏷️ [HotFix] 빌드 시 발생하는 `gray-matter` 타입 컴파일 이슈를 수정한다

--- src/layers/shared/lib/types/index.ts
+++ src/layers/shared/lib/types/index.ts
@@ -21,21 +21,3 @@ export type TMetadata = {
   tags: Array<string>;
   thumbnail: string;
 };
-
-declare module 'gray-matter' {
-  function matter<
-    D,
-    I extends Input,
-    O extends GrayMatter.GrayMatterOption<I, O>,
-  >(input: I | { content: I }, options?: O): GrayMatter.GrayMatterFile<D, I>;
-
-  interface GrayMatterFile<D, I extends Input> {
-    data: D;
-    content: string;
-    excerpt?: string;
-    orig: Buffer | I;
-    language: string;
-    matter: string;
-    stringify(lang: string): string;
-  }
-}
