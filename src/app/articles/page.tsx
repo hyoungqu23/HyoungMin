d717e9cd1cafb68d0e8541acb8b63f927df95282
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-11 22:11:17 +0900

    💄 [#17] ArticlesPage 슬라이스를 활용해 ArticlesPage의 UI를 수정한다

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,21 +1,10 @@
-import { notFound } from 'next/navigation';
 import { articlesService } from '@entities';
+import { ArticlesPage } from '@pages';
 
-const ArticlesPage = () => {
+const Articles = () => {
   const articles = articlesService.get();
 
-  return (
-    <section className='flex flex-col items-center justify-between px-4 py-10 tablet:py-40'>
-      <section className='flex flex-wrap gap-8 w-full justify-center'>
-        {/* {articleService.sortByCreatedAt().map((article) => (
-          <Card
-            key={article._id}
-            articlePreview={articleService.getPreview(article)}
-          />
-        ))} */}
-      </section>
-    </section>
-  );
+  return <ArticlesPage articles={articles} />;
 };
 
-export default ArticlesPage;
+export default Articles;
