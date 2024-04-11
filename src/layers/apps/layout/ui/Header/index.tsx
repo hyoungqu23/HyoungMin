0378aca487bf1fba14d0000b368770723bc10f1a
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-11 21:41:07 +0900

    💄 [#17] Header에서 카테고리 페이지를 제거한다

--- src/layers/apps/layout/ui/Header/index.tsx
+++ src/layers/apps/layout/ui/Header/index.tsx
@@ -5,15 +5,17 @@ export const Header = () => {
   return (
     <header className='sticky z-50 top-0 flex items-center justify-center w-screen h-16 bg-secondary-900/50 backdrop-blur-sm bg-clip-padding backdrop-filter'>
       <nav className='flex gap-8'>
-        {Object.values(NAVIGATION_ITEMS).map((item) => (
-          <Link
-            key={item.id}
-            href={item.route}
-            className='block px-6 py-4 font-semibold tracking-wider text-white transition-all duration-500 text-heading6 hover:text-primary-500 active:scale-95'
-          >
-            {item.id.toUpperCase()}
-          </Link>
-        ))}
+        {Object.values(NAVIGATION_ITEMS)
+          .filter((item) => item.id !== NAVIGATION_ITEMS.CATEGORY.id)
+          .map((item) => (
+            <Link
+              key={item.id}
+              href={item.route}
+              className='block px-6 py-4 font-semibold tracking-wider text-white transition-all duration-500 text-heading6 hover:text-primary-500 active:scale-95'
+            >
+              {item.id.toUpperCase()}
+            </Link>
+          ))}
       </nav>
     </header>
   );
