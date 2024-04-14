4878d89eb6e03adc8911e026a37bf63dbdd086b6
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-14 10:31:49 +0900

    🚑 [HotFix] 날짜 포맷팅 함수를 추가하고 이를 적용해 배포 서버 위치로 인한 날짜 포맷팅 이슈를 수정한다

--- src/layers/shared/lib/utils/index.ts
+++ src/layers/shared/lib/utils/index.ts
@@ -22,3 +22,11 @@ export const capitalizeFirstLetter = (str: string) =>
   str.charAt(0).toUpperCase() + str.slice(1);
 
 export const getRootDirectoryPath = () => process.cwd();
+
+export const format = (date: Date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}년 ${month}월 ${day}일`;
+};
