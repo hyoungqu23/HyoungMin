6dba3e5e56c7928c45fd7ec8913d9aed533af32d
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-04-18 23:36:03 +0900

    🚑 [HotFix] Category 페이지의 렌더링 방식을 Static Site Generation으로 변경한다

--- src/app/articles/about/[category]/page.tsx
+++ src/app/articles/about/[category]/page.tsx
@@ -6,9 +6,27 @@ interface ICategoryPageProps {
 }
 
 const CategoryPage = ({ params: { category } }: ICategoryPageProps) => {
+  console.log('✅ category', category);
+
   const articlesInCategory = articlesService.filter(category);
+
+  console.log('✅ articlesInCategory', articlesInCategory);
 
   return <ArticlesPage articles={articlesInCategory} />;
 };
 
 export default CategoryPage;
+
+export const generateStaticParams = () => {
+  const params = [
+    ...new Set(
+      articlesService.get().map((article) => ({
+        category: article.category.toLowerCase().replaceAll('.', ''),
+      })),
+    ),
+  ];
+
+  console.log('✅ params', params);
+
+  return params;
+};
