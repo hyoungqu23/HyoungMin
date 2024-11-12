9a42797ee2b28284a34df1bf0bef404e334e7fbe
HyoungMin <hyoungqu23@gmail.com>
2024-11-13 08:39:30 +0900

    💬 [HotFix] Production BASE_URL을 변경한다

--- src/layers/shared/asset/constants.ts
+++ src/layers/shared/asset/constants.ts
@@ -1,6 +1,6 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://hyoungmin2.vercel.app'
+    ? 'https://hyoungmin.vercel.app'
     : 'http://localhost:3000';
 
 export const NAVIGATION_ITEMS = {
