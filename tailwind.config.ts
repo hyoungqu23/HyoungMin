74c5c8946621b3d9b5405908c481679e5c89dff7
HyoungMin <hyoungqu23@gmail.com>
2025-01-04 09:03:57 +0900

    [#21] FSD 아키텍처 구조에 맞는 Tailwind CSS Config의 content를 설정한다

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,14 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./src/layers/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './src/root/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/widgets/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/entities/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/shared/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     screens: {
       minimum: '375px',
