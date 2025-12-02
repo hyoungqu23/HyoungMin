2aadf24b3257620d043d0d2992501c5a33edbf87
HyoungMin <hyoungqu23@gmail.com>
2025-12-03 01:29:28 +0900

    feat: 메타데이터 업데이트

--- apps/wedding/src/app/layout.tsx
+++ apps/wedding/src/app/layout.tsx
@@ -1,12 +1,43 @@
 import type { Metadata } from "next";
-import "./globals.css";
-import { Splash } from "./_components/Splash";
-import { FloatingButton } from "./_components/FloatingButton";
 import { Analytics } from "./_components/Analytics";
+import { FloatingButton } from "./_components/FloatingButton";
+import { Splash } from "./_components/Splash";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Wedding Day",
-  description: "Hyoungmin and Heejea's Wedding Day",
+  title: "이형민 ♥ 임희재 결혼식",
+  description: "형민이와 희재의 결혼식에 초대합니다.",
+  openGraph: {
+    title: "이형민 ♥ 임희재 결혼식",
+    description: "형민이와 희재의 결혼식에 초대합니다.",
+    images: [
+      {
+        url: "/images/sample.jpg",
+        width: 1200,
+        height: 630,
+        alt: "이형민 ♥ 임희재 결혼식",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "이형민 ♥ 임희재 결혼식",
+    description: "형민이와 희재의 결혼식에 초대합니다.",
+    images: [
+      {
+        url: "/images/sample.jpg",
+        width: 1200,
+        height: 630,
+        alt: "이형민 ♥ 임희재 결혼식",
+      },
+    ],
+  },
+  alternates: {
+    canonical: "/",
+  },
+  // verification: {},
 };
 
 type LayoutProps = Readonly<{
@@ -16,7 +47,7 @@ type LayoutProps = Readonly<{
 const RootLayout = ({ children }: LayoutProps) => {
   return (
     <html lang="ko">
-      <body className="antialiased">
+      <body className="antialiased bg-rose-50">
         <Analytics />
         <Splash />
         {children}
