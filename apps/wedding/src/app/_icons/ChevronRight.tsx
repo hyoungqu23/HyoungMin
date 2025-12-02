607344f92daa312f6da2cca848d7107c839fda61
HyoungMin <hyoungqu23@gmail.com>
2025-12-03 01:27:43 +0900

    feat: 아이콘 추가

---
+++ apps/wedding/src/app/_icons/ChevronRight.tsx
@@ -0,0 +1,12 @@
+export const ChevronRight = () => (
+  <svg
+    width="40"
+    height="40"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+  >
+    <path d="M9 18l6-6-6-6" />
+  </svg>
+);
