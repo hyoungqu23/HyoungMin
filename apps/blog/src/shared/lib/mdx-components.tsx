43c25c58dcfa35ee56939fa29a5664eaa45ab3a6
HyoungMin <hyoungqu23@gmail.com>
2025-12-15 15:56:12 +0900

    feat: Add script to generate MDX components and update package.json scripts

--- apps/blog/src/shared/lib/mdx-components.tsx
+++ apps/blog/src/shared/lib/mdx-components.tsx
@@ -2,7 +2,10 @@ import { CodeBlock, MdxImage } from "@hyoungmin/ui";
 import type { MDXRemoteProps } from "next-mdx-remote/rsc";
 import type { ComponentProps } from "react";
 
+import { mdxWidgetComponents } from "./mdx-widgets.generated";
+
 export const mdxComponents: MDXRemoteProps["components"] = {
   pre: (props: ComponentProps<"pre">) => <CodeBlock {...props} />,
   img: (props: ComponentProps<"img">) => <MdxImage {...props} />,
+  ...mdxWidgetComponents,
 };
