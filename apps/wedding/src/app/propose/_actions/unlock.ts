0f0addcdd370754326f83987b44153c6ffe79183
HyoungMin <hyoungqu23@gmail.com>
2026-01-19 08:43:04 +0900

    chore: 코드 정리

--- apps/wedding/src/app/propose/_actions/unlock.ts
+++
@@ -1,8 +0,0 @@
-const FALLBACK_UNLOCK_CODE = "151226";
-
-export const PROPOSE_UNLOCK_CODE =
-  process.env.NEXT_PUBLIC_PROPOSE_UNLOCK_CODE ?? FALLBACK_UNLOCK_CODE;
-
-export const isUnlockCodeValid = (code: string) => {
-  return code.trim() === PROPOSE_UNLOCK_CODE;
-};
